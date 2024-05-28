ecc0586829f25d218c4011ce09e1fbb7a3ed6bb6
WallacX <88462062+WallacX@users.noreply.github.com>
2024-05-28 16:00:16 -0300

    Atividas 1° semana
    Distribuição das atividades para os membros

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+//Funções Somatório PIC e GUC
+//Função Conversão Binário/Decimal
+//Menu (switch & do while) - Entrada nas opções e volta pro menu
+//Interface - Seguindp os padrões especificados no pdf
+//Manipulação de arquivos em C - Escrita e Leitura
+
+
 #include <stdio.h>
 
 int main(){
 
+  printf("Olá mundo");
 
-
-
+return 0;
 }
