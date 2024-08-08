4df63d7b08efbf083d572d1b3a9d89162536ea5a
WallacX <88462062+WallacX@users.noreply.github.com>
2024-08-08 16:09:39 -0300

    Create interface.h

---
+++ interface.h
@@ -0,0 +1,33 @@
+//Aqui ficam armazenados os protótipos das funções que serão utilizadas no interface.c
+
+void Menu();
+
+void Entrada();
+
+void Carregamento();
+
+void EstatisticasMensais();
+
+int TxtExtensao(const char *);
+
+void ListaTxt(const char *);
+
+void ZeraVetor(int [], int);
+
+float CalculaPic(float [], int [], int);
+
+float CalculaGuc(float [], int [] , float [],int);
+
+float CalculaToneladas(float [], int);
+
+float CalculaPesoLimpo(float [], int [], int);
+
+void Cabecalho(int, int);
+
+void RelatorioCarga(int, int, int, float, float, int, int, int [], int, int [], int, int [], int , int [], int);
+
+void RelatorioMensal(int, int);
+
+void RelatorioGeral(int);
+
+void Rodape();
