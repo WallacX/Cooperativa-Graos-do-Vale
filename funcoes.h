49c69ddbb606dde57edd34cc4b9490b958db6ebf
WallacX <88462062+WallacX@users.noreply.github.com>
2024-08-08 16:10:12 -0300

    Create funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,23 @@
+//Aqui ficam armazenados os protótipos das funções que serão utilizadas no funcoes.c 
+
+void Menu();
+
+void Entrada();
+
+void Carregamento();
+
+void EstatisticasMensais();
+
+int TxtExtensao(const char *);
+
+void ListaTxt(const char *);
+
+void ZeraVetor(int [], int);
+
+float CalculaPic(float [], int [], int);
+
+float CalculaGuc(float [], int [] , float [],int);
+
+float CalculaToneladas(float [], int);
+
+float CalculaPesoLimpo(float [], int [], int);
